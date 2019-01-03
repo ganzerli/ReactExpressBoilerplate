4374a86202a8a5e6bd9ea849cfa5c97bbb5e1561
Massimo Ganzerli <massimoganzerli@yahoo.com>
2019-01-03 21:52:31 +0100

    adding post functionality

--- server.js
+++ server.js
@@ -5,32 +5,40 @@ const express = require("express");
 //initialize the server
 const app = express();
 
+//just inventing an array
+const responseArray = [
+  //filling the array with objects
+  { id: "1", prop2: "value2", usefulData: "DATA 1" },
+  { id: "2", prop2: "value2", usefulData: "DATA 2" },
+  {
+    id: "3",
+    prop2: "value2",
+    usefulData: "DATA 3"
+  },
+  { id: "4", prop2: "value2", usefulData: "DATA 4" },
+  {
+    id: "5",
+    prop2: "value2",
+    usefulData: "DATA 5"
+  }
+];
+
 //adding a route where to listen
 app.get("/api/array", (req, res) => {
   //creating the response
-  //just inventing an array
-  const ResponseArray = [
-    //filling the array with objects
-    { id: "1", prop2: "value2", usefuleData: "DATA 1" },
-    { id: "2", prop2: "value2", usefuleData: "DATA 2" },
-    {
-      id: "3",
-      prop2: "value2",
-      usefuleData: "DATA 3"
-    },
-    { id: "4", prop2: "value2", usefuleData: "DATA 4" },
-    {
-      id: "5",
-      prop2: "value2",
-      usefuleData: "DATA 5"
-    }
-  ];
   //parsing the array to output as response
-  res.json(ResponseArray);
+  res.json(responseArray);
+});
+// accessing to request parameters
+app.get("/api/params/:id", (req, res) => {
+  res.json(req.params);
 });
 
+app.post("api/array", (req, res) => {});
+// environmant variable
+const environmentPort = process.env.PORT;
 // giving a port number port 3000 is the default or create react app.
-const port = 5000;
+const port = environmentPort || 5000;
 
 // give the server the port to listen and start the server
 app.listen(port, () => console.log(`Server started on port ${port}`));
